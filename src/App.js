d7f191aeeb5235bd474ca4ec99fd3a0faf53aa97
Вадим Валыгин <vadim12388@mail.ru>
2023-06-28 13:10:57 +0300

    setting linter Makefile

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-function App() {
-  return (
-    <div> </div>
-  );
-}
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const App = () => (
+  <div className="wrapper">
+    <Header />
+    <Footer />
+  </div>
+);
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <div>
+      <span className="logo">House Stuff</span>
+    </div>
+    <div className="presentation" />
+  </header>
+);
+
+export default Header;
